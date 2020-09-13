d21baddcc96183899606069a2778aefe144f650b
LetMeDream <raulalfonzo66@gmail.com>
2020-09-13 08:34:30 -0400

    Start setting messenger's widget

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,18 +89,7 @@
 
 </div>
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/5e9dd34a35bcbb0c9ab2f968/default';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
+<!-- This site is converting visitors into subscribers and customers with respond.io - https://respond.io -->
+<script src="https://app.respond.io/facebook/chat/plugin/40880/181956451966108" async></script>
 
 @endsection
